566d871447c40c792b436f3ac4c7fa18e3ae690a
Mark Agimba <markaguero16@gmail.com>
2019-03-05 14:36:03 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ var player2="";
 
 var rolldice = function () {
   return Math.floor(6*Math.random())+1;
-}
+};
 
 function player(turn) {
-  this.roll =0
-  this.accumulatingtotal = 0
-  this.overalltotal = 0
+  this.roll =0;
+  this.accumulatingtotal = 0;
+  this.overalltotal = 0;
   this.turn = turn;
   this.playerName;
 }
@@ -18,12 +18,12 @@ function player(turn) {
 Player.prototype.rollone = function () {
   if (this.roll === 1) {
   this.accumulatingtotal = 0;
-  alert("Ahhh, " + this.playerName + "well well, you scored 1! Better Luck next turn!")
+  alert("Ahhh, " + this.playerName + "well well, you scored 1!; Better Luck next turn!");
   // this.switchturn();
   } else {
   this.accumulatingtotal += this.roll;
   }
-}
+};
 
 // Hold
 Player.prototype.hold= function () {
@@ -31,7 +31,7 @@ Player.prototype.hold= function () {
   this.accumulatingtotal = 0;
   // this.switchturn();
   alert(this.playerName + "Kindly pass the mouse over to next player!");
-}
+};
 
 // // switching turn
 // Player.prototype.switchturn = function () {
@@ -47,81 +47,81 @@ Player.prototype.winnerCheck = function () {
   if (this.overalltotal >= 100) {
     alert(this.playerName + " Congratulations, YOU WON!");
   }
-}
+};
 Player.prototype.newGame = function () {
   //debugger;
   this.roll = 0;
   this.accumulatingtotal = 0;
   this.overalltotal = 0;
   this.playerName ="";
-}
+};
 
 var clearValues = function(){
-  $(".player1Name").val("");
-  $(".player2Name").val("");
-}
+  (".player1Name").val("");
+  (".player2Name").val("");
+};
 
 //USER INTERFACE LOGIC
-$(document).ready(function() {
+(document).ready(function() {
 
-  $("button#play").click(function(event){
+  ("button#play").click(function(event){
     player1 = new Player(true);
     player2 =  new Player(false);
-    $(".player-console").show();
-    $(".start-menu").hide();
+    (".player-console").show();
+    (".start-menu").hide();
 
-    var player1Name =$(".player1Name").val();
-    $("#player1Name").text(player1Name);
+    var player1Name =(".player1Name").val();
+    ("#player1Name").text(player1Name);
 
-    var player2Name = $(".player2Name").val();
-    $("#player2Name").text(player2Name);
+    var player2Name = (".player2Name").val();
+    ("#player2Name").text(player2Name);
 
     player1.playerName=player1Name;
     player2.playerName=player2Name;
 
   });
-  $("button#new-game").click(function(event){
-    $(".player-console").hide();
+  ("button#new-game").click(function(event){
+    (".player-console").hide();
     clearValues();
     player1.newGame();
     player2.newGame();
-    $("#accumulating-total-1").empty();
-    $("#overall-total-1").empty();
-    $("#die-roll-1").empty();
-    $("#accumulating-total-2").empty();
-    $("#overall-total-2").empty();
-    $("#die-roll-2").empty();
+    ("#accumulating-total-1").empty();
+    ("#overall-total-1").empty();
+    ("#die-roll-1").empty();
+    ("#accumulating-total-2").empty();
+    ("#overall-total-2").empty();
+    ("#die-roll-2").empty();
 
-    $(".start-menu").show();
+    (".start-menu").show();
   });
 
-  $("button#player1-roll").click(function(event){
+  ("button#player1-roll").click(function(event){
 player1.roll = rolldice();
-   $("#die-roll-1").text(player1.roll);
+   ("#die-roll-1").text(player1.roll);
    player1.rollone();
-   $("#accumulating-total-1").text(player1.accumulatingtotal);
+   ("#accumulating-total-1").text(player1.accumulatingtotal);
  });
 
- $("button#player2-roll").click(function(event){
+ ("button#player2-roll").click(function(event){
     player2.roll = rolldice();
-    $("#die-roll-2").text(player2.roll);
+    ("#die-roll-2").text(player2.roll);
     player2.rollone();
-    $("#accumulating-total-2").text(player2.accumulatingtotal);
+    ("#accumulating-total-2").text(player2.accumulatingtotal);
   });
 
- $("button#player1-hold").click(function(event){
+ ("button#player1-hold").click(function(event){
     player1.hold();
-    $("#overall-total-1").text(player1.overalltotal);
-    $("#accumulating-total-1").empty();
-    $("#die-roll-1").empty();
+    ("#overall-total-1").text(player1.overalltotal);
+    ("#accumulating-total-1").empty();
+    ("#die-roll-1").empty();
     player1.winnerCheck();
   });
 
-  $("button#player2-hold").click(function(event){
+  ("button#player2-hold").click(function(event){
     player2.hold();
-    $("#overall-total-2").text(player2.overalltotal);
-    $("#accumulating-total-2").empty();
-    $("#die-roll-2").empty();
+    ("#overall-total-2").text(player2.overalltotal);
+    ("#accumulating-total-2").empty();
+    ("#die-roll-2").empty();
     player2.winnerCheck();
   });
 

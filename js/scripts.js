8f54fcc0d62efb4cc967592c444fe5e1d0eca483
Mark Agimba <markaguero16@gmail.com>
2019-03-05 10:58:16 +0300

    business interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,31 @@ Player.prototype.hold= function () {
   // this.switchturn();
   alert(this.playerName + "Kindly pass the mouse over to next player!");
 }
+
+// // switching turn
+// Player.prototype.switchturn = function () {
+//   if (this.roll ===1) {
+//     this.turn = false;
+//   } else {
+//     this.turn = true;
+//   }
+//   }
+// }
+// check for 100
+Player.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " Congratulations, YOU WON!");
+  }
+}
+Player.prototype.newGame = function () {
+  //debugger;
+  this.roll = 0;
+  this.accumulatingtotal = 0;
+  this.overalltotal = 0;
+  this.playerName ="";
+}
+
+var clearValues = function(){
+  $(".player1Name").val("");
+  $(".player2Name").val("");
+}
